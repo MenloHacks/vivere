a8a17130601e6714731bc82d1dd2a99e83cd37d0
Jason Scharff <jasonscharff@gmail.com>
2017-02-21 14:03:57 -1000

    construct models

--- vivere.py
+++ vivere.py
@@ -1,31 +1,51 @@
+
+import datetime
+
 from flask import Flask
-from flask_superadmin import Admin, model
-from mongoengine import *
+
+import flask_admin as admin
+from flask_mongoengine import MongoEngine
+from flask_admin.form import rules
+from flask_admin.contrib.mongoengine import ModelView
 
 # Create application
 app = Flask(__name__)
 
-# Create dummy secret key so we can use sessions
+# Create dummy secrey key so we can use sessions
 app.config['SECRET_KEY'] = '123456790'
+app.config['MONGODB_SETTINGS'] = {'DB': 'testing'}
 
-connect('menlohacks')
-# db = MongoEngine(app)
+# Create models
+db = MongoEngine()
+db.init_app(app)
 
-
-# Defining MongoEngine Documents
-
-class Event(Document):
-    start_time = DateTimeField()
-    end_time = DateTimeField()
-
-    short_description = StringField()
-    long_description = StringField()
+class Location(db.Document):
+    name = db.StringField()
+    map = db.FileField()
 
     def __unicode__(self):
-        return self.short_description
+        return self.name
 
 
+# Define mongoengine documents
+class Event(db.Document):
+    start_time = db.DateTimeField()
+    end_time = db.DateTimeField()
 
+    short_description = db.StringField()
+    long_description = db.StringField()
+
+    location = db.ReferenceField(Location, required=False)
+
+
+    def __unicode__(self):
+        return self.long_description
+
+
+class Announcement(db.Document):
+    title = db.StringField()
+    contents = db.StringField()
+    time = db.DateTimeField(default=datetime.datetime.now())
 
 # Flask views
 @app.route('/')
@@ -35,16 +55,12 @@ def index():
 
 if __name__ == '__main__':
     # Create admin
-    admin = Admin(app)
+    admin = admin.Admin(app, 'MenloHacks Vivere')
 
-    class EventModel(model.ModelAdmin):
-        list_display = ('start_time', 'end_time', 'short_description', 'long_description')
-
-
-    # Register the models
-    admin.register(Event, EventModel)
+    # Add views
+    admin.add_views(ModelView(Announcement))
+    admin.add_view(ModelView(Event))
+    admin.add_views(ModelView(Location))
 
     # Start app
-    app.debug = True
-    app.run()
-
+    app.run(debug=True)
