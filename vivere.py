7177a48f28fba856b8249cfe3f40b9e7d20cebfc
Jason Scharff <jasonscharff@gmail.com>
2017-02-23 11:56:01 -1000

    Change port and IP

--- vivere.py
+++ vivere.py
@@ -49,6 +49,6 @@ if __name__ == '__main__':
     admin.add_views(ModelView(MentorTicket))
 
 
-
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
