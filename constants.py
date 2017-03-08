b075011f797b93d1da86a2c296c5d78f8eaab084
Jason Scharff <jasonscharff@gmail.com>
2017-03-07 23:03:38 -0800

    Remember DST is a thing

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 import datetime
 
-EVENT_START_TIME = datetime.datetime(year=2017, month=3, day=18, hour=0, minute=15)
-EVENT_END_TIME = datetime.datetime(year=2017, month=3, day=19, hour=2)
+EVENT_START_TIME = datetime.datetime(year=2017, month=3, day=17, hour=23, minute=15)
+EVENT_END_TIME = datetime.datetime(year=2017, month=3, day=19, hour=1)
 
-HACKING_START_TIME = datetime.datetime(year=2017, month=3, day=18, hour=2)
-HACKING_END_TIME = datetime.datetime(year=2017, month=3, day=19, hour=0)
+HACKING_START_TIME = datetime.datetime(year=2017, month=3, day=18, hour=1)
+HACKING_END_TIME = datetime.datetime(year=2017, month=3, day=18, hour=23)
 
 AUTHORIZATION_HEADER_FIELD = 'X-MenloHacks-Authorization'
 
