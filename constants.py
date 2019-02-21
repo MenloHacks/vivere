a6cda27428cdf2fdc36d0c7d73dcf8d9ead8b577
Mitsuka <mitsuka.kiyohara@menloschool.org>
2019-02-21 12:31:13 -0800

    updated time

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 import datetime
 
-EVENT_START_TIME = datetime.datetime(year=2018, month=3, day=10, hour=10)
-EVENT_END_TIME = datetime.datetime(year=2018, month=3, day=11, hour=14, minute=15)
+EVENT_START_TIME = datetime.datetime(year=2019, month=3, day=9, hour=10)
+EVENT_END_TIME = datetime.datetime(year=2019, month=3, day=10, hour=3)
 
-HACKING_START_TIME = datetime.datetime(year=2018, month=3, day=10, hour=12)
-HACKING_END_TIME = datetime.datetime(year=2018, month=3, day=11, hour=12)
+HACKING_START_TIME = datetime.datetime(year=2019, month=3, day=9, hour=12)
+HACKING_END_TIME = datetime.datetime(year=2019, month=3, day=10, hour=12)
 
 AUTHORIZATION_HEADER_FIELD = 'X-MenloHacks-Authorization'
 ADMIN_HEADER_FIELD = 'X-MenloHacks-Admin'
